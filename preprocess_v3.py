fdf53a657d9982bb5a6df4ccb826624fdbc1fa71
moon1863 <ralam@knights.ucf.edu>
2020-12-05 23:18:09 -0500

    SOC and GT and ChargeNeeded identification done

--- preprocess_v3.py
+++ preprocess_v3.py
@@ -26,26 +26,26 @@ import pandas as pd
 # temp=(df_2['start_date'].value_counts().head(100))#datewise top 100 trip number generated 
 # temp.to_csv("day_vs_trip.csv")# opened in excel and check the day 
 # df_2['start_date']=pd.to_datetime(df_2['start_date'])
-# df_3_15_17=df_2[df_2['start_date']=="2017-03-15"]#wednesday with max trip
+# df_4_5_17=df_2[df_2['start_date']=="2017-04-05"]#wednesday with max trip
 # 
 # #generate cumulative minutes from the beginning of day
-# df_3_15_17['start_minute']=df_3_15_17['started_on'].dt.minute
-# df_3_15_17['start_hr']=df_3_15_17['started_on'].dt.hour
-# df_3_15_17['start_cum_minute']=(df_3_15_17['start_hr']*60)+(df_3_15_17['start_minute'])
-# df_3_15_17.to_csv("df_3_15_17.csv")
+# df_4_5_17['start_minute']=df_4_5_17['started_on'].dt.minute
+# df_4_5_17['start_hr']=df_4_5_17['started_on'].dt.hour
+# df_4_5_17['start_cum_minute']=(df_4_5_17['start_hr']*60)+(df_4_5_17['start_minute'])
+# df_4_5_17.to_csv("df_4_5_17.csv")
 # =============================================================================
 
 #you can start from here. ABove commented section are the base.NEVER DELETE
-df_3_15_17=pd.read_csv("df_3_15_17.csv")
+df_4_5_17=pd.read_csv("df_4_5_17.csv")
 
 #set up coordinate
-subset_strt_location = df_3_15_17[['start_location_lat','start_location_long']]
-df_3_15_17['coord_start'] = [tuple(x) for x in subset_strt_location.to_numpy()]
+subset_strt_location = df_4_5_17[['start_location_lat','start_location_long']]
+df_4_5_17['coord_start'] = [tuple(x) for x in subset_strt_location.to_numpy()]
 
-subset_end_location = df_3_15_17[['end_location_lat','end_location_long']]
-df_3_15_17['coord_end'] = [tuple(x) for x in subset_end_location.to_numpy()]
+subset_end_location = df_4_5_17[['end_location_lat','end_location_long']]
+df_4_5_17['coord_end'] = [tuple(x) for x in subset_end_location.to_numpy()]
 
-df_3=df_3_15_17.groupby(['driver_id'])['model'].first()
+df_3=df_4_5_17.groupby(['driver_id'])['model'].first()
 df_3=df_3.to_frame()
 
 #assign random model for FV
@@ -63,7 +63,7 @@ for i in range (0,len(df_3)):
     df_3.iloc[i]=df_3.iloc[i].replace(to_replace=FV_model_li,value=model_replacer)
     
 df_3=df_3.add_suffix('').reset_index()
-df_4=df_3_15_17.merge(df_3,left_on="driver_id",
+df_4=df_4_5_17.merge(df_3,left_on="driver_id",
                       right_on="driver_id")  #model_y denotes randomly converted EV model  
 
 #assign EV properties 
@@ -94,45 +94,22 @@ df_5.drop('Unnamed: 0',axis=1,inplace=True)
 # =============================================================================
 
 ##find lambda i.e., distance factor
+from functions import distance
+df_5.insert(31,"shortest_dist","")#extra colum created with unit km
 
-# distance using Haversine formula
-import math
-from math import *
-def distance(loc_i,loc_f):
-    R = 6373.0
-    
-    lat1 = radians(loc_i[0])
-    lon1 = radians(loc_i[1])
-    lat2 = radians(loc_f[0])
-    lon2 = radians(loc_f[1])
-    
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    
-    return R * c
+for i in range (0,len(df_5)):#shortest path distance estimated
+    df_5['shortest_dist'][i]=distance(df_5['coord_start'][i],df_5['coord_end'][i])
 
+df_5['distance_travelled']=df_5['distance_travelled']/1000#converted to km
 
+df_6=df_5.groupby(['driver_id']).agg({'distance_travelled':sum,
+                                      'shortest_dist':sum})
+df_6=df_6.add_suffix('').reset_index()
+df_6['lambda']=df_6['distance_travelled']/df_6['shortest_dist']
 
+#create df with id,coord and time
+df_7=df_5[['driver_id','coord_start','coord_end','start_cum_minute']]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
 #create_pivot_table
 pivot=pd.pivot_table(df_7,index=['driver_id'],columns=['start_cum_minute'],\
                values=['coord_start'],aggfunc="first")
@@ -144,15 +121,20 @@ future_headers_2=list(set(future_headers_1))
 future_headers_2.sort()
 pivot.columns=future_headers_2
 
-
-
 #pivot to dictionary
 pivot=pivot.reset_index()#add driver_id as column
-pivot_dic=pivot.set_index('driver_id').T.to_dict()#now, coord is tuple
 
- 
+col_name=list(range(0,1440))
+temp_pivot=pd.DataFrame(columns=col_name)
 
-    
+concatenated = pd.concat([pivot, temp_pivot], axis=0)
+
+driverid=concatenated['driver_id']
+concatenated.drop('driver_id',axis=1,inplace=True)
+concatenated.insert(0,'driver_id',driverid)
+pivot2=concatenated
+pivot_dic=pivot2.set_index('driver_id').T.to_dict()#dictionary with driver key and another dic of time:coord as values
+
 #sample data 
 # =============================================================================
 # sample_dic={108:[0,(30.265,-97.732),0,0,0,(30.319,-97.738),0,(30.5,-97.737)]}
@@ -184,27 +166,23 @@ pivot_dic=pivot.set_index('driver_id').T.to_dict()#now, coord is tuple
 #     dist_list.append(dist[1]/(list_position[2]-list_position[1]))  
 # =============================================================================
 
-
 dri_df=pd.DataFrame.from_dict(pivot_dic, orient='index')#convert first column as index
 dri_df.fillna(0,inplace=True)
 dri_df_col_li=dri_df.columns.tolist()
-
-
-
+dri_df=dri_df.reset_index()#note: if you use add suffix"" then the column name will be string
+dri_df.rename(columns={'index':'driver_id'},inplace=True)
 
 #######DISTANCE
-
-li=list(range(0,1440))
+li=dri_df_col_li#old one (errorful we will use if needed later): li=list(range(0,len(dri_df_col_li)))
 dict_li=[]
-#d=0#started from 0 to 26 we need use loop, we will loop do it later
-for d in range(0,26):
+for d in range(0,100):#for full data, use len(dri_df)
     list_coordinate={}
     list_position=[]
     for i in dri_df_col_li:
         if dri_df.iloc[d][i] !=0:
             list_position.append(i)
             list_coordinate[i]=dri_df.iloc[d][i]
-        else: dri_df.iloc[d][i]=dri_df.iloc[d][i]    
+        #else: dri_df.iloc[d][i]=dri_df.iloc[d][i]    
     
     
     dist=[]#distance only for points with coord
@@ -220,45 +198,54 @@ for d in range(0,26):
         dist_list.append(0)
     
     for i in range(0,len(list_position)-1):     #find distance for all minutes
-        for j in range(list_position[i]+1,list_position[i+1]+1):    
-            dist_list.append(dist[i]/(list_position[i+1]-list_position[i]))
+        for j in range(list_position[i]+1,(list_position[i+1])+1):    
+            dist_list.append(dist[i]/((list_position[i+1])-(list_position[i])))
     
-    for i in range (list_position[-1],1439):    
+    for i in range (list_position[-1],len(dri_df_col_li)):    
         dist_list.append(0)        
     
     for i in range(0,len(dist_list)-1):         #find cumulative distance              
         dist_list[i+1]=dist_list[i]+dist_list[i+1]
     
     dict_li.append(dict(zip(li,dist_list)))
-#dictionary_0=dict(zip(li,dist_list)) #combining all dictionary_driver
-#loop will end up here
 
-# =============================================================================
-# dict_li=[dictionary_0,dictionary_1,dictionary_2,dictionary_3,dictionary_4,dictionary_5,
-# dictionary_6,dictionary_7,dictionary_8,dictionary_9,dictionary_10,dictionary_11,dictionary_12,
-# dictionary_13,dictionary_14,dictionary_15,dictionary_16,dictionary_17,dictionary_18,
-# dictionary_19,dictionary_20,dictionary_21,dictionary_22,dictionary_23,dictionary_24,
-# dictionary_25]
-# =============================================================================
-
-distance_dict=dict(zip(list(dri_df.index),dict_li)) #combined with corresponding driver id
+distance_dict=dict(zip(list(dri_df.driver_id),dict_li)) #combined with corresponding driver id
         
-
 cum_distance_df=pd.DataFrame.from_dict(distance_dict, orient='index')
 cum_distance_df.to_csv("cum_distance_df.csv")
 cum_distance_df_2=cum_distance_df.reset_index()
-####################
+cum_distance_df_2.rename(columns={'index':'driver_id'},inplace=True)
 
 
+#distance converted to routing distance using lambda
+df_6_100=df_6.head(100)
+cum_distance_df_3=cum_distance_df_2.merge(df_6_100,left_on="driver_id",right_on="driver_id")
 
+for i in range(0,100):
+    for j in range (1,1441):
+        cum_distance_df_3.iloc[i,j]=(cum_distance_df_3.iloc[i,j])*(cum_distance_df_3.iloc[i,1443]) 
+
+cum_distance_df_4=cum_distance_df_3.drop(columns=["distance_travelled","shortest_dist","lambda"])
+
+
+#generate SOC
+df_5_1=df_5[['driver_id','capacity','MKWH']].drop_duplicates()
+soc=cum_distance_df_4.merge(df_5_1,left_on="driver_id",right_on="driver_id")
+
+for i in range(0,100):
+    for j in range (1,1441):
+        soc.iloc[i,j]=100-(((soc.iloc[i,j])*0.62)/((soc.iloc[i,1442])*(soc.iloc[i,1441])))*100 
+
+soc.drop(columns=["MKWH","capacity"],inplace=True)
+soc.to_csv("SOC.csv")
 ##########time interval from immediate next trip 
-def time_diff(x,y):
-    return x-y
-s=0
-p=['']*26
+from functions import time_diff
 
-for d in range (0,26):
-    m=[0]*1440#blank list
+s=0
+p=['']*100 #for full data, replace 150 with len(dri_df)
+
+for d in range (0,100): #for full data, replace 150 with len(dri_df)
+    m=[0]*len(li)#blank list
     
     list_position_ti=[]
     for i in dri_df_col_li:
@@ -278,9 +265,10 @@ for d in range (0,26):
     gap=[] 
     for i in range(0,len(list_position_ti)): #find the gap value
         try:
-            gap.append(time_diff(m[list_position_ti[i+1]],m[list_position_ti[i]]))          
+            gap.append(time_diff(m[list_position_ti[i+1]],m[list_position_ti[i]]))
         except IndexError:
             break
+    gap.append(0)
     
     j=0
     for i in range(0,len(m)):
@@ -342,53 +330,55 @@ for d in range (0,26):
 #         dist_list.append(dist[i]/(list_position[i+1]-list_position[i]))
 # =============================================================================
 
-dictionary_time=dict(zip(list(dri_df.index),p))
-
+dictionary_time=dict(zip(list(dri_df.driver_id),p))
 
 time_intv_df=pd.DataFrame.from_dict(dictionary_time, orient='index')
-time_intv_df.to_csv("time_intv_df.csv")
 time_intv_df_2=time_intv_df.reset_index()
-
+time_intv_df_2.rename(columns={"index":"driver_id"},inplace=True)
+time_intv_df_2.to_csv("time_intv_df_2.csv")
 #SOC and GT comparison
-SOC=pd.read_csv("SOC1440.csv")
-SOC.drop("Unnamed: 0",1,inplace=True)
+soc=pd.read_csv("SOC.csv")
+soc.drop(columns=["Unnamed: 0"],inplace=True)
 
-GT=pd.read_csv("time_intv_df.csv")
-
+gt=pd.read_csv("time_intv_df_2.csv")
+gt.drop(columns=["Unnamed: 0"],inplace=True)
 
 list_number=list(range(0,1440))
 l=[str(x) for x in list_number]
 
-SOC.iloc[:,1:]=SOC.iloc[:,1:].astype(float)
+#soc.iloc[:,1:]=soc.iloc[:,1:].astype(float)
     
 for i in range (1,1440):
-    SOC.loc[(SOC.iloc[:,i]<0.5)&(GT.iloc[:,i]>60),l[i-1]] = "CN"
+    soc.loc[(soc.iloc[:,i]<50)&(gt.iloc[:,i]>20),l[i-1]] = "CN"
     
-SOC.to_csv("CN_SOC_GTT.csv")    
+soc.to_csv("CN_SOC_GT.csv")    
 
 aa=pd.read_csv("CN_SOC_GTT.csv")    
 
 
-a=list(dri_df.columns)
-a=[str(x) for x in a]
-
-dri_df_str_col=dri_df.copy()
-dri_df_str_col.columns=a
-dri_df_str_col=dri_df_str_col.reset_index()
-
-
-aa2=aa[list(dri_df_str_col.columns)]#df with CN mention and same size with dri_df_str_col
-
-
-aa2.insert(loc=0, column='driver_id', value=list(aa['index']))
-
-
-#modified df particular drivers
-
-matched_driver=[70,1418,2483,2648,2862,3241,3662,4172,4442,4656]
-
-aa2_mdf=aa2.loc[aa2['driver_id'].isin(matched_driver)]
-dri_df_str_col_mdf=dri_df_str_col.loc[dri_df_str_col['index'].isin(matched_driver)]
+# =============================================================================
+# Useless
+#a=list(dri_df.columns)
+# a=[str(x) for x in a]
+# 
+# dri_df_str_col=dri_df.copy()
+# dri_df_str_col.columns=a
+# dri_df_str_col=dri_df_str_col.reset_index()
+# 
+# 
+# aa2=aa[list(dri_df_str_col.columns)]#df with CN mention and same size with dri_df_str_col
+# 
+# 
+# aa2.insert(loc=0, column='driver_id', value=list(aa['index']))
+# 
+# 
+# #modified df particular drivers
+# 
+# matched_driver=[70,1418,2483,2648,2862,3241,3662,4172,4442,4656]
+# 
+# aa2_mdf=aa2.loc[aa2['driver_id'].isin(matched_driver)]
+# dri_df_str_col_mdf=dri_df_str_col.loc[dri_df_str_col['index'].isin(matched_driver)]
+# =============================================================================
 
 
 # =============================================================================
